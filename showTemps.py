94ec591eedfc259bca7f9ea244c05f4f9100dd45
blm <bruce.mclean@unfoldingword.org>
2023-09-05 07:18:43 -0400

    minimize data reads

--- showTemps.py
+++ showTemps.py
@@ -35,8 +35,11 @@ def read_file(file_path):
 def read_last_line(file_path, chunk_size=128):
     try:
         with open(filename, 'r') as file:
+            print("opened", file_path)
             file.seek(-128, 2)  # Move the pointer 128 characters from the end of the file
+            print("seeked", file_path)
             data = file.read()
+            print("read", file_path)
             if data:
                 lines = data.split('\n')
                 return lines[-1] if lines else None
